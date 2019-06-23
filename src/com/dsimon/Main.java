2c3c4e58f9d431826a5f4fe68e97b91fbdfb22a4
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-22 23:33:15 -0400

    challenge done

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -3,6 +3,9 @@ package com.dsimon;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(LastDigitChecker.hasSameLastDigit(41, 22, 71) == true);
+        System.out.println(LastDigitChecker.hasSameLastDigit(23, 32, 42) == true);
+        System.out.println(LastDigitChecker.hasSameLastDigit(9, 99, 999) == false);
+        System.out.println(LastDigitChecker.hasSameLastDigit(94, 33, 22) == false);
     }
 }
